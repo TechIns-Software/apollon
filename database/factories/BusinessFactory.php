16aec1db2a75607138c41ec7854058603524a3d3
pcmagas <pcmagas@disroot.org>
2024-03-22 10:22:58 +0200

    AO-32 Business Factory

--- database/factories/BusinessFactory.php
+++ database/factories/BusinessFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Facades\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,13 +19,22 @@ class BusinessFactory extends Factory
     {
         return [
             'name'=>fake()->name,
-            'expiration_date'=>null,
+            'expiration_date'=>Carbon::now()->modify("+1 year"),
             'active'=>true
         ];
     }
 
-    public function active()
+    public function inactive()
     {
+        return $this->afterMaking(function (Business $business){
+           $business->active = false;
+        });
+    }
 
+    public function expired()
+    {
+        return $this->afterMaking(function (Business $business){
+            $business->expiration_date = (new Carbon())->modify("-2 days");
+        });
     }
 }
