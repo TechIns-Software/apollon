6c7f1d9072c6ca803e0c442dd9ff62cfb324f021
pcmagas <pcmagas@disroot.org>
2024-04-09 14:29:28 +0300

    AO-10 Base Insert Controller.

--- tests/Feature/Controllers/API/DeliveryControllerTest.php
+++ tests/Feature/Controllers/API/DeliveryControllerTest.php
@@ -2,8 +2,10 @@
 
 namespace Feature\Controllers\API;
 
+use App\Models\Business;
 use App\Models\Delivery;
 use App\Models\DeliveryOrder;
+use App\Models\Driver;
 use App\Models\Order;
 use App\Models\SaasUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -34,6 +36,64 @@ class DeliveryControllerTest extends TestCase
             'name'=>'Panzer Delivery',
             'orders'=>$orderIds
         ];
+
+        $response = $this->post('/api/delivery',$payload);
+        $body=$response->json();
+        $response->assertStatus(201);
+
+        $delivery=Delivery::find($body['id']);
+
+        $driver = Driver::find($delivery->driver_id);
+        $this->assertNotEmpty($driver);
+        $this->assertEquals($user->business_id,$driver->business_id);
+
+        $this->assertNotEmpty($delivery);
+        $this->assertEquals($user->business_id,$delivery->business_id);
+        $this->assertEquals($body['business_id'],$delivery->business_id);
+        $this->assertEquals($payload['name'],$delivery->name);
+
+        foreach ($body['orders'] as $order) {
+            $order=$order['order'];
+            $deliveryOrder = DeliveryOrder::whereOrderId($order['id'])
+                ->whereDeliveryId($delivery->id)
+                ->first();
+
+            $this->assertNotEmpty($deliveryOrder);
+            $order = Order::find($deliveryOrder->order_id);
+
+            $this->assertNotEmpty($order);
+            $this->assertEquals($deliveryOrder->id,$order->id);
+            $this->assertEquals($user->business_id,$order->business_id);
+        }
+    }
+
+    public function testAddWithExistingDriver()
+    {
+        $user = SaasUser::factory()->create();
+        $orders = Order::factory(5)->withUser($user)->withProducts()->create();
+
+        $driver = Driver::create([
+            'driver_name'=>'lalalala',
+            'business_id'=>$user->business_id,
+        ]);
+
+        $orderIds = [];
+        foreach ($orders as $order) {
+            $orderIds[] = $order->id;
+        }
+
+        Sanctum::actingAs(
+            $user,
+            ['mobile_api']
+        );
+
+        $payload=[
+            'driver_id'=>$driver->id,
+            'delivery_date'=>'2025-12-01',
+            'name'=>'Panzer Delivery',
+            'orders'=>$orderIds
+        ];
+
 
         $response = $this->post('/api/delivery',$payload);
         $body=$response->json();
