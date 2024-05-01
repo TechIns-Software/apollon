335bc9a1ee3c8a77341be4ee14a1de2b862ce0cf
pcmagas <pcmagas@disroot.org>
2024-05-01 12:45:55 +0300

    AO-37 deployment

---
+++ deploy/application-start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+sudo -u www-data php /var/www/html/artisan up
+
+service nginx reload
+service nginx start
