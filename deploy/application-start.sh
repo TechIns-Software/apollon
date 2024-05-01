a114fb1d012598dcfd0c1ee03b0e29c098d58750
pcmagas <pcmagas@disroot.org>
2024-05-01 15:28:02 +0300

    AO-36 Fix Vhost.

--- deploy/application-start.sh
+++ deploy/application-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default.conf
 sudo -u www-data php /var/www/html/app/artisan up
 
 service nginx start
