2465c0bb64b11ffeb4d6b4c0c7bbbc8de0f59c5b
pcmagas <pcmagas@disroot.org>
2024-05-01 14:29:13 +0300

    AO-37 Fix Nginx

--- deploy/application-start.sh
+++ deploy/application-start.sh
@@ -3,5 +3,5 @@
 ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
 sudo -u www-data php /var/www/html/artisan up
 
-service nginx reload
 service nginx start
+service nginx reload
